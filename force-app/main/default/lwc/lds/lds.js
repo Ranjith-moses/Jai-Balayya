bae6d0465610a5343f0d2c267d020c8b27b1c18d
Ranjith <mosedevara@gmail.com>
2023-02-07 11:36:17 +0530

    ju

--- force-app/main/default/lwc/lds/lds.js
+++ force-app/main/default/lwc/lds/lds.js
@@ -1,8 +1,9 @@
 import { LightningElement } from 'lwc';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import bulmacss from '@salesforce/resourceUrl/bulmacss';
-
+import bulmavideo from '@salesforce/resourceUrl/bgvideo';
 export default class Lds extends LightningElement {
+    sfvideo = bulmavideo;
     connectedCallback() {
         loadStyle(this, bulmacss);
     }
